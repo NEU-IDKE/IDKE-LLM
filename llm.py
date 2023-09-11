788c7fbe77ab708391cb66be619348fe349257b3
LookingYu <141821448+LookingYu@users.noreply.github.com>
2023-09-11 19:47:50 +0800

    Update llm.py

--- llm.py
+++ llm.py
@@ -275,8 +275,6 @@ def page4():
             st.caption(description)
             st.write("参数需求")
             st.write(paramters)
-            st.write("test") # test
-            st.write(param for param in paramters) # test
             st.session_state.paramters = paramters
         else:
             st.session_state.paramters = {}
