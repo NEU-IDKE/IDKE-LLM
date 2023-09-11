966a7631a32643c760c217f972a2b21f9d41f3bb
yanyi74 <116535337+yanyi74@users.noreply.github.com>
2023-09-11 19:24:17 +0800

    Update llm.py
    工具包切换错误

--- llm.py
+++ llm.py
@@ -275,6 +275,8 @@ def page4():
             st.caption(description)
             st.write("参数需求")
             st.write(paramters)
+            st.session_state["messages_t"] = []
+            st.session_state["messages_length_t"] = 0
             st.session_state.paramters = paramters
         else:
             st.session_state.paramters = {}
